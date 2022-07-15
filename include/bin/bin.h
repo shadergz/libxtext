64fb41a60c88f4099466dcf4117140bee8d0f73d
Gabriel Correia <gabrielcorreia01012020@hotmail.com>
2022-07-15 13:56:47 -0300

    Making the basic structure

--- include/bin/bin.h
+++ include/bin/bin.h
@@ -3,13 +3,28 @@
 
 #include "bintypes.h"
 
-/* Load a binary file from his pathname */
+/* -------- Load a binary file from his pathname -------- */
 bin_err_e bin_load_file(const char *pathname, bin_ctx_t *bin);
 
-/* Unload the current file */
+/* -------- Unload the current file -------- */
 bin_err_e bin_unload_file(bin_ctx_t *bin);
 
-/* Cleaning the context structure */
+/* -------- Parser the executable -------- */
+bin_err_e bin_parser(bin_ctx_t *bin);
+
+/* -------- Getting functions -------- */
+
+/* The returned pointer by bin_get_filename is allocated using malloc,
+ * after call bin_finish function, this pointer will be destroyed.
+*/
+const char* bin_get_filename(const bin_ctx_t *bin);
+const bin_err_e bin_get_last_error(const bin_ctx_t *bin);
+size_t bin_get_binary_size(const bin_ctx_t *bin);
+
+/* -------- Error handler functions -------- */
+const char* bin_error_to_str(const bin_err_e error_value);
+
+/* -------- Cleaning the context structure -------- */
 bin_err_e bin_finish(bin_ctx_t *bin);
 
 #endif
--- include/bin/bintypes.h
+++ include/bin/bintypes.h
@@ -2,6 +2,7 @@
 #define LIBBIN_BINTYPES_H
 
 #include <stdbool.h>
+#include <stdint.h>
 
 typedef int fd_t;
 typedef int flags_t;
@@ -13,12 +14,38 @@ typedef enum
     BIN_E_OPEN_FILE,
 
     /* Can't close the file, maybe has been deleted */
-    BIN_E_CLOSE_FILE
+    BIN_E_CLOSE_FILE,
+
+    /* A recently allocation using the malloc function has been failed */
+    BIN_E_MALLOCATION_ERROR,
+
+    /* The supposed binary file is empty */
+    BIN_E_IS_EMPTY,
+
+    /* The executable has been alredy parsed */
+    BIN_E_ALREDY_PARSED,
+
+#if defined(__unix__)
+    /* A recently call for function fstat has returned a fail value */
+    BIN_E_FSTAT_FAILED,
+
+    /* A recently call for mmap has failed */
+    BIN_E_MMAP_FAILED,
+
+    /* A recently call for munmap has failed */
+    BIN_E_MUNMAP_FAILED,
+
+#endif
+
+    BIN_E_FINAL_NULL_VALUE
 
 } bin_err_e;
 
 typedef struct
 {
+    /* Binary file pathname */
+    char *pathname;
+
 #if defined(__unix__)
     fd_t fd;
 #if _POSIX_C_SOURCE >= 200809L
@@ -27,8 +54,23 @@ typedef struct
     int internal_errno;
     flags_t fd_flags;
 #endif
+
+    size_t binary_file_size;
+
     /* A internal error status value (allow the getting method) */
     bin_err_e error_status;
+
+    union {
+        struct {
+#if defined(__unix__)
+            unsigned char *map_start;
+            uintptr_t map_end;
+            size_t map_size;
+#endif
+        } mapped_area;
+
+        bool using_mapped;
+    };
 
 } bin_ctx_t;
 
